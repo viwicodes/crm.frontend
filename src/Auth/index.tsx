dc7fd2e7160bb0af4e053700670db1bba7617232
viwicodes <viwicodes@gmail.com>
2024-01-10 19:03:36 +0530

    complete login page

--- src/Auth/index.tsx
+++ src/Auth/index.tsx
@@ -1,90 +1,165 @@
-import React, { useEffect, useState } from 'react';
-import {
-    Grid,
-    TextField,
-    Paper,
-    Button
-} from '@mui/material';
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { useState, useEffect } from "react";
+
+import styles from "./style.module.scss";
+
 const LoginPage = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [data, setData] = useState([]);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [data, setData] = useState([]);
 
-    // useEffect(() => {
-    const fetchData = () => {
-        // Replace 'yourBearerToken' with your actual bearer token
+  // useEffect(() => {
+  const fetchData = () => {
+    // Replace 'yourBearerToken' with your actual bearer token
 
-        fetch('http://localhost:4000/api/auth/login', {
-            credentials: 'same-origin',
-            
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                username: username,
-                password: password,
-            }),
-        })
-            .then((response) => {
-                // Check if the request was successful (status code 2xx)
-                if (!response.ok) {
-                    throw new Error(`HTTP error! Status: ${response.status}`);
-                }
+    fetch("http://localhost:4000/api/auth/login", {
+      credentials: "same-origin",
 
-                // Parse the response JSON
-                const cookie = response.headers
-                console.log('myCookie:', cookie);
-                return response.json();
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    })
+      .then((response) => {
+        // Check if the request was successful (status code 2xx)
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
 
-            })
-            .then((result) => {
-                console.log(result,'cook')
-                // const myCookieValue = result.headers['set-cookie'];
-            })
-            .catch((error) => {
-                // Handle errors
-            })
-            .finally(() => {
-                // Set loading to false when the API call is complete
-            });
-    };
+        // Parse the response JSON
+        const cookie = response.headers;
+        console.log("myCookie:", cookie);
+        return response.json();
+      })
+      .then((result) => {
+        console.log(result, "cook");
+        // const myCookieValue = result.headers['set-cookie'];
+      })
+      .catch((error) => {
+        // Handle errors
+      })
+      .finally(() => {
+        // Set loading to false when the API call is complete
+      });
+  };
 
-    // Call the fetchData function when the component mounts
-    const onClick = () => {
-        fetchData()
-    }
-    // }, []);
+  // Call the fetchData function when the component mounts
+  const onClick = () => {
+    fetchData();
+  };
+  // }, []);
 
-    const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-        setUsername(event.target.value);
-    };
-    const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-        setPassword(event.target.value);
-    };
+  const handleUsernameChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setUsername(event.target.value);
+  };
+  const handlePasswordChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setPassword(event.target.value);
+  };
 
-    return (
-        <div style={{ padding: 30 }}>
-            <Paper>
-                <Grid>
-                    <Grid item xs={12}>
-                        <TextField
-                            label="Username"
-                            value={username}
-                            onChange={(e) => handleUsernameChange(e)}></TextField>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <TextField
-                            value={password}
-                            onChange={(e) => handlePasswordChange(e)} label="Password" type={'password'}></TextField>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <Button fullWidth onClick={onClick}>Login</Button>
-                    </Grid>
-                </Grid>
-            </Paper>
-        </div>
-    );
+  return (
+    <div className={styles.main}>
+      <Container
+        id="main-container"
+        component="main"
+        maxWidth="xs"
+        className={styles.formBox}
+      >
+        <CssBaseline />
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <img src="assets/logo.svg" alt="usica temporary logo" />
+          <Typography
+            className={styles.welcomeText}
+            component="h1"
+            variant="h5"
+          >
+            Hi, Welcome back
+          </Typography>
+          <Box
+            component="form"
+            // onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Username"
+              name="username"
+              autoComplete="email"
+              autoFocus
+              value={username}
+              onChange={(e) => handleUsernameChange(e)}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              value={password}
+              onChange={(e) => handlePasswordChange(e)}
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              onClick={onClick}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </div>
+  );
 };
 
 export default LoginPage;
